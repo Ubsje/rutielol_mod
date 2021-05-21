bd76fea9da9a24d9ff86aafef59dd21f70a0e296
rubenoke <Ruben@DESKTOP-C9GC783>
2021-05-21 16:22:08 +0200

    Manexvallei geadd

---
+++ src/main/java/net/mcreator/rutielolmod/block/ManexLeavesBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.rutielolmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.rutielolmod.RutielolModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@RutielolModModElements.ModElement.Tag
+public class ManexLeavesBlock extends RutielolModModElements.ModElement {
+	@ObjectHolder("rutielol_mod:manex_leaves")
+	public static final Block block = null;
+	public ManexLeavesBlock(RutielolModModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid());
+			setRegistryName("manex_leaves");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 1;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
