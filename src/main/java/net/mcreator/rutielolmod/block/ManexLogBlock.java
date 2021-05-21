4d291de801e753b204676ab14251cdaa661b83b0
Ubsje <s1158932@student.windesheim.nl>
2021-05-21 16:38:48 +0200

    Synch

--- src/main/java/net/mcreator/rutielolmod/block/ManexLogBlock.java
+++ src/main/java/net/mcreator/rutielolmod/block/ManexLogBlock.java
@@ -1,17 +1,36 @@
 
 package net.mcreator.rutielolmod.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.rutielolmod.RutielolModModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @RutielolModModElements.ModElement.Tag
 public class ManexLogBlock extends RutielolModModElements.ModElement {
-
 	@ObjectHolder("rutielol_mod:manex_log")
 	public static final Block block = null;
-
 	public ManexLogBlock(RutielolModModElements instance) {
 		super(instance, 6);
-
 	}
 
 	@Override
@@ -20,17 +39,12 @@ public class ManexLogBlock extends RutielolModModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public static final DirectionProperty FACING = DirectionalBlock.FACING;
-
 		public CustomBlock() {
 			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1.1f, 10f).setLightLevel(s -> 0).harvestLevel(0)
 					.harvestTool(ToolType.AXE).setRequiresTool());
-
 			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.SOUTH));
-
 			setRegistryName("manex_log");
 		}
 
@@ -65,13 +79,10 @@ public class ManexLogBlock extends RutielolModModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
